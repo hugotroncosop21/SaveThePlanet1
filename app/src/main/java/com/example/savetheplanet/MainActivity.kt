ddec70880976ea8ed5690b2cfa8e76326e7e984b
hugotroncosop <hugo.troncoso.pombal@fpfomento.com>
2025-02-04 19:19:52 +0100

    Actualización a la versión 2.0

--- app/src/main/java/com/example/savetheplanet/MainActivity.kt
+++ app/src/main/java/com/example/savetheplanet/MainActivity.kt
@@ -1,28 +1,122 @@
 package com.example.savetheplanet
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.savetheplanet.ui.theme.SaveThePlanetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SaveThePlanetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+        setContent{//Lo de dentro seran Composable
+        ScaffoldExample()
+        }
+    }
+}
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("SAVE THE PLANET")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "END",
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(top = 50.dp)) {
+            /*
+            Image(
+                painter = painterResource(id = R.drawable.descarga),
+                contentDescription = "Descripción de la imagen",
+                modifier = Modifier.fillMaxWidth()
+            )
+            */
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Aplicación sin ánimo de lucro, con el objetivo de concienciar y mostrar los cambios que sufre nuestro planeta",
+                modifier = Modifier.padding(horizontal = 16.dp),
+                color = Color.DarkGray
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            val datos = listOf(
+                "1. Energía y emisiones",
+                "2. Impactos sobre la naturaleza",
+                "3. Adaptación y mitigación",
+                "4. Políticas y sostenibilidad"
+            )
+            LazyColumn(modifier = Modifier.padding(16.dp)) {
+                items(datos) { item ->
+                    Text(
+                        text = item,
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        color = Color.Blue
                     )
                 }
             }
@@ -30,18 +124,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SaveThePlanetTheme {
-        Greeting("Android")
-    }
-}
